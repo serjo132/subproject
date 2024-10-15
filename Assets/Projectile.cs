3403d8066938e4d36b1491f821f673dd4731046c
sepj <serzh.matusevich@mail.ru>
2024-10-15 13:32:12 +0300

    finished

---
+++ Assets/Projectile.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float moveSpeed;
+    public GameObject explosionPrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+        if(collision.gameObject.tag == "Boundary")
+        {
+            Destroy(gameObject);
+        }    
+    }
+}
